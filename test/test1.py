20e39fb2475b7f8a51e0d7655684d1e272bbaf94
zhangjiang.huang <huang.zhangjiang@mwee.cn>
2023-08-16 18:13:47 +0800

    up

---
+++ test/test1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+ 
+print("Hello, World!")
+print( "你好，世界" )
+
+if True:
+    print ("Answer")
+    print ("True")
+else:
+    print ("Answer")
+    # 没有严格缩进，在执行时会报错
+print ("False")
